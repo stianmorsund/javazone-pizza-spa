c93e344fc671abfacf3a073c68a88d2d0352e2f1
stianmorsund <stian.morsund@webstep.no>
2022-08-23 22:03:23 +0200

    style

--- apps/routing/pages/index.tsx
+++ apps/routing/pages/index.tsx
@@ -1,8 +1,5 @@
-import styles from './index.module.css';
-import Link from 'next/link';
-
 export function Index() {
-  return <h1 className="text-4xl">Home</h1>;
+  return <h1>Home</h1>;
 }
 
 export default Index;
