b52be26e8d1763aa737ca6ab39c29d3f7107c80e
stianmorsund <stian.morsund@webstep.no>
2022-08-23 21:27:26 +0200

    use nextjs instead

--- apps/routing/jest.config.ts
+++ apps/routing/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: '01-routing',
+  displayName: 'routing',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
-    '^.+\\.[tj]sx?$': 'babel-jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/01-routing',
+  coverageDirectory: '../../coverage/apps/routing',
 };
