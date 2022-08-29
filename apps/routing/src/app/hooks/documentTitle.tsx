4e02006cccc742ed11b33c1cfbdc4bbcc6e0ac1c
stianmorsund <stian.morsund@webstep.no>
2022-08-29 22:12:02 +0200

    delete effect

--- apps/routing/src/app/hooks/documentTitle.tsx
+++
@@ -1,8 +0,0 @@
-import { useEffect } from 'react'
-export const useDocumentTitle = (title: string) => {
-  useEffect(() => {
-    document.title = `${title} - WebsiteName`
-  }, [title])
-
-  return null
-}
