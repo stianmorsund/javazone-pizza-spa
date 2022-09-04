7bc68c6225769a028e777ca592a463d56de92070
stianmorsund <stian.morsund@webstep.no>
2022-09-04 20:08:38 +0200

    liveannouncher as function

--- apps/routing/src/app/announcher.tsx
+++ apps/routing/src/app/announcher.tsx
@@ -1,19 +1,18 @@
-import React from 'react'
-
-export class LiveAnnouncher extends React.Component<{
+export function LiveAnnouncher({
+  message,
+  ariaLive,
+}: {
   message: string | undefined
   ariaLive: 'polite' | 'assertive'
-}> {
-  override render() {
-    return (
-      <div
-        className="screenreader-only"
-        role="status"
-        aria-live={this.props.ariaLive || 'polite'}
-        aria-atomic="true"
-      >
-        {this.props.message}
-      </div>
-    )
-  }
+}) {
+  return (
+    <div
+      className="screenreader-only"
+      role="status"
+      aria-live={ariaLive || 'polite'}
+      aria-atomic="true"
+    >
+      {message}
+    </div>
+  )
 }
