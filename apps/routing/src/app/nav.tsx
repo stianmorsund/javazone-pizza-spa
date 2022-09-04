463d28a5ec7462a258fce88f78329f5387626577
stianmorsund <stian.morsund@webstep.no>
2022-09-04 19:31:13 +0200

    use function and NavLink

--- apps/routing/src/app/nav.tsx
+++ apps/routing/src/app/nav.tsx
@@ -1,30 +1,32 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { useEffect } from 'react'
+import { NavLink } from 'react-router-dom'
 import { IRoute } from './utils/route'
 
-export class NavigationMenu extends React.Component<{
+export function NavigationMenu({
+  routes,
+  currentPage,
+}: {
   routes: IRoute[]
   currentPage: string | undefined
-}> {
-  override componentDidUpdate() {
-    // document.title = `${this.props.currentPage} - Pizza`
-    // document.body.focus()
-    // window.scrollTo(0, 0)
-  }
-  override render() {
-    return (
-      <div>
-        <img src="assets/logo.svg" alt="Logo" className="logo" />
-        <ul>
-          {this.props.routes.map((route, index) => {
-            return (
-              <li key={index}>
-                <Link to={route.path}>{route.niceName}</Link>
-              </li>
-            )
-          })}
-        </ul>
-      </div>
-    )
-  }
+}) {
+  useEffect(() => {
+    document.title = `${currentPage} - Pizza Ipsum`
+    document.body.focus()
+    window.scrollTo(0, 0)
+  })
+
+  return (
+    <div>
+      <img src="assets/logo.svg" alt="Logo" className="logo" />
+      <ul>
+        {routes.map((route, index) => {
+          return (
+            <li key={index}>
+              <NavLink to={route.path}>{route.niceName}</NavLink>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
 }
